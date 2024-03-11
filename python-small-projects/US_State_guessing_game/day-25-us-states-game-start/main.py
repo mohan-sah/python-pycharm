d09407786fc546d0024610360977a522955c37e7
mohan-sah <mohansah944@gmail.com>
2024-03-11 21:25:22 +0530

    us state guess game

---
+++ python-small-projects/US_State_guessing_game/day-25-us-states-game-start/main.py
@@ -0,0 +1,71 @@
+import turtle
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+tim = turtle.Turtle()
+
+
+# To get (x,y) of particular click.
+# def get_mouse_click_coor(x,y):
+#     print(x,y)
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+def show_on_map(answer_state_name, x, y):
+    tim.shapesize(0.4, 0.4)
+    tim.penup()
+    tim.shape("circle")
+    tim.goto(x, y)
+    tim.write(f"{answer_state_name}", font=("Arial", 8, "normal"))
+
+
+states = pd.read_csv("50_states.csv")
+state_names = states["state"].to_list()
+
+correct_answer = 0
+all_answered = True
+answered_list = []
+while all_answered:
+
+    answer_state = screen.textinput(title=f"{correct_answer}/50 Guess the State",
+                                    prompt="What's another state's name? ").lower()
+
+    for name in state_names:
+        # print(name, states[states.state == f"{name}"].x, states[states.state == f"{name}"].y)
+        if answer_state == "exit":
+            all_answered = False
+            break
+        if name.lower() == answer_state:
+            current_state = states[states.state == name]
+
+            if answer_state not in answered_list:
+                answered_list.append(answer_state)
+                correct_answer += 1
+                show_on_map(str(name), current_state.x.item(), current_state.y.item())
+                if correct_answer == 50:
+                    all_answered = False  # means game finished
+                    print("YOU WON")
+                    print("Correctly answered all 50 states")
+
+            else:
+                correct_answer += 0
+
+# states_to_learn.csv  contain names user may want to look.
+for name in answered_list:
+    print(name)
+    if name in answered_list[0]:
+        state_names.pop()
+print(len(state_names))
+
+
+df = pd.DataFrame(state_names)
+df.to_csv("states_to_learn.csv")
+
+#
+# turtle.mainloop()
