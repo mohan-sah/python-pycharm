644238e3bd7763fe64eb6ce8dedce909d3c6be3e
mohan-sah <mohansah944@gmail.com>
2024-03-17 16:08:47 +0530

    miles to km converter converter.

---
+++ python-small-projects/mile_to_km_converter/main.py
@@ -0,0 +1,6 @@
+# label, is equal to  = 1,0
+# label, miles = 0,2
+# label, Km  = 1,2
+# input , num = 0,1
+# display , num = 1,1
+# button , calculate =  2,1
--- python-small-projects/mile_to_km_converter/tinkering_with_Tkinter/main.py
+++ python-small-projects/mile_to_km_converter/tinkering_with_Tkinter/main.py
@@ -3,7 +3,8 @@ from tkinter import *
 
 window = Tk()
 window.title("GUI PROGRAM")
-window.minsize(width=500,height=300)
+window.minsize(width=500, height=300)
+window.config(padx=100, pady=100)
 
 # *args for arguments where * is important.all arguments are in tuple form
 # which then can be looped through.
@@ -17,38 +18,35 @@ window.minsize(width=500,height=300)
 
 
 # label
-my_label = Label(text="I am a Label", font=("Ariel", 24,"bold"))
+my_label = Label(text="I am a Label", font=("Ariel", 24, "bold"))
 
 # # passing value to dict as it is a **kwargs OR pass as keyword argument
 # my_label["text"] = "new text"
 my_label.config(text=" New text")
-my_label.pack()
-
+my_label.grid(column = 0, row = 0)
+my_label.config(padx= 50, pady=50)
 
 
 # button
 def button_clicked():
     text = input.get()
     my_label.config(text=text)
-    my_label.pack()
+    my_label.grid(column =0, row = 0)
     print("i got clicked")
 
 
-button = Button(text = "click me" , command=button_clicked)
-button.pack()
 
+button = Button(text="click me", command=button_clicked)
+button.grid(column = 1, row = 1)
+
+button2 = Button(text="click me 2", command=button_clicked)
+button2.grid(column = 2, row = 0)
 # Entry
 
-input = Entry(width= 15)
-input.pack()
+input = Entry(width=10)
+input.grid(column = 3 ,row = 2)
 
-
-
-
-
-
-
-
+# PACK - (stacking) PLACE-(corrdinate system) GRID
 
 
 window.mainloop()
