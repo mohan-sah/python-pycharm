3649fc086aa059f6cacf5b04b50f5d5fb6e82667
mohan-sah <mohansah944@gmail.com>
2024-05-03 17:36:39 +0530

    password-manager ui and functionality

---
+++ python-small-projects/password-manager/main.py
@@ -0,0 +1,102 @@
+from tkinter import *
+from tkinter import messagebox
+from password_generator import PasswordGenerator
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+password_generator = PasswordGenerator()
+
+print(password_generator.password_list)
+def action_generate():
+    entry_password.delete(0,END)
+    temp = password_generator.generate()
+    entry_password.insert(END, temp)
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+# in file data.txt
+def action_save():
+    print("Do something")
+    website = entry_website.get()
+    email = entry_email.get()
+    password = entry_password.get()
+
+    if len(website) == 0 or len(password) == 0:
+        should_save = False
+        messagebox.showinfo(title="CANNOT BE SAVED" , message="textbox not filled")
+    else:
+        should_save = messagebox.askokcancel(title=f"{website}", message=f"Details submitted : \n Email : {email}\n Password : {password}\n click ok to save")
+    if should_save:
+        with open("data.txt" , "a") as f:
+            f.write(f"\n{website} | {email} | {password}")
+
+
+
+    #emptying fields
+    entry_website.delete(0,END)
+    entry_password.delete(0,END)
+
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+#title : Password Manager
+#width: 200, padding : 20, Height : 200
+
+window = Tk()
+
+window.title('Password Manager')
+window.config(padx=20 , pady=20)
+
+canvas = Canvas(width=200, height=200 )
+lock_img = PhotoImage(file="logo.png")
+canvas.create_image(100,100, image = lock_img)
+canvas.grid(column = 1, row = 0)
+
+#Labels - website
+label_website = Label(text="Website :")
+label_website.grid(column = 0, row = 1)
+
+#Labels - email/username
+label_email = Label(text="Email/Username :")
+label_email.grid(column = 0, row = 2)
+
+#Labels-password
+label_password = Label(text="Password :")
+label_password.grid(column = 0, row = 3)
+
+#Entries - website
+entry_website = Entry(width=35 )
+entry_website.focus()
+entry_website.grid(column = 1, row =1 , columnspan =2 )
+
+#Entries - email
+entry_email = Entry(width=35)
+entry_email.insert(END , "mohansah944@gmail.com")
+entry_email.grid(column = 1, row =2  ,columnspan = 2)
+
+#Entries - password
+entry_password = Entry(width=25)
+entry_password.grid(column = 1, row =3)
+
+#Button - generate
+#calls action() when pressed
+button_generate_password = Button(text="Generate Password", command=action_generate)
+button_generate_password.grid(column = 2, row = 3)
+
+#Button - add
+#calls action() when pressed
+button_add = Button(text="Add", command=action_save , width=30)
+button_add.grid(column = 1, row = 4 , columnspan  = 2)
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
+
+
---
+++ python-small-projects/password-manager/password_generator.py
@@ -0,0 +1,48 @@
+# Password Generator Project
+import random
+
+
+class PasswordGenerator(object):
+    def __init__(self):
+        self.letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                        't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        self.numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        self.symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+        # print("Welcome to the PyPassword Generator!")
+        self.nr_letters = random.randint(8, 10)
+        self.nr_symbols = random.randint(2, 4)
+        self.nr_numbers = random.randint(2, 4)
+        self.password_list = None
+
+    # Eazy Level - Order not randomised:
+    # e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+    # password = ""
+    # for letter in range(0,nr_letters):
+    #   password += random.choice(letters)
+    # for symbol in range(0,nr_symbols):
+    #   password += random.choice(symbols)
+    # for number in range(0,nr_numbers):
+    #   password += random.choice(numbers)
+    # password = ''.join(random.sample(password,len(password)))
+
+    # print(f"Your password is {password}")
+    # random_list = [random_letters,random_numbers,random_symbols]
+    # length_random_list = len(random_list) -1
+
+    # Hard Level - Order of characters randomised:
+    # e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+    def generate(self):
+        password_list = []
+        for letter in range(0, self.nr_letters):
+            password_list += random.choice(self.letters)
+        for symbol in range(0, self.nr_symbols):
+            password_list += random.choice(self.symbols)
+        for number in range(0, self.nr_numbers):
+            password_list += random.choice(self.numbers)
+        # print(f"Your password is {password_list}")
+        random.shuffle(password_list)
+        self.password_list = ''.join(password_list)
+        return self.password_list
