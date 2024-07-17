d90ed5414953ca0791eb6a156c94bd17b96e10e8
mohan-sah <mohansah944@gmail.com>
2024-07-17 21:42:48 +0530

    change of dir

--- python-small-projects/snake_game/test_import_export/main.py
+++ python-small-projects/snake_game/test_import_export/main.py
@@ -2,7 +2,7 @@ with open("../../../../../Desktop/new_file.txt") as file:
     contents = file.read()
     print(contents)
 # "/Users/mohan/Desktop/new_file.txt"
-# C:\Users\mohan\PycharmProjects\python-pycharm\python-small-projects\snake_game\test_import_export\main.py
+# C:\Users\mohan\PycharmProjects\python-pycharm\python-small-projects\snake_game\test_import_export\side.py
 # so we go five files up and at same level we change the boat to Desktop and go down from there.
 # with open("new_file.txt", mode="w") as file:
 #     file.write("\nI appear magically")
