53333a8da7441ebf89caf262c603e08bc817aad2
mohan-sah <mohansah944@gmail.com>
2024-07-31 17:40:02 +0530

    update iatacode in google sheets if empty from flight data apis

--- python-small-projects/flight-deals-alert/data_manager.py
+++ python-small-projects/flight-deals-alert/data_manager.py
@@ -1,3 +1,62 @@
+import requests
+import pandas as pd
+from pprint import pprint
+from dotenv import load_dotenv
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MY_ENV_VAR = os.getenv('MY_ENV_VAR')
+SHEETY_AUTHORIZATION = os.getenv('SHEETY_AUTHORIZATION')
+
+SHEETY_ENDPOINT = "https://api.sheety.co/d5cbe021794f22c079cba500da92b423/flightPrices/sheet1"
+headers = {
+    "Authorization": SHEETY_AUTHORIZATION
+}
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    """This class is responsible for talking to the Google Sheet."""
+
+    def __init__(self):
+        self.parameters = None
+        self.response = None
+        self.datarow = None
+        self.SHEETY_ENDPOINT = SHEETY_ENDPOINT
+
+        self.headers = {
+            "Authorization": SHEETY_AUTHORIZATION
+        }
+
+    def getdata(self):
+        self.response = requests.get(url=SHEETY_ENDPOINT, headers=headers)
+        self.response.raise_for_status()
+        self.datarow = self.response.json()['sheet1']
+        return self.datarow
+
+    def edit_exiting_record(self):
+        for city in self.datarow:
+            self.parameters = {
+                "sheet1": {"city": city["city"],
+                           "iataCodes": city["iataCodes"],
+                           }
+            }
+            self.response = requests.put(url=f"{SHEETY_ENDPOINT}/{city['id']}", headers=headers, json=self.parameters)
+            self.response.raise_for_status()
+
+
+# data_manager = DataManager()
+# data_manager.edit_exiting_record()
+
+# #This class is responsible for talking to the Google Sheet.
+# SHEETY_ENDPOINT = "https://api.sheety.co/d5cbe021794f22c079cba500da92b423/flightPrices/sheet1"
+# headers = {
+#         "Authorization": "Basic ZmxpZ2h0X3ByaWNlczpqazQ4cndxOWVhZnVzZGlvam4jZGo="
+#     }
+#
+# response =  requests.get(url=SHEETY_ENDPOINT, headers=headers)
+# response.raise_for_status()
+# data = response.json()
+# pprint(data)
--- python-small-projects/flight-deals-alert/main.py
+++ python-small-projects/flight-deals-alert/main.py
@@ -1,1 +1,21 @@
+from data_manager import DataManager
+from flight_search import FlightSearch
+
+data_manager = DataManager()
+sheet_data = data_manager.getdata()
+print(sheet_data)
+
+flight_search = FlightSearch()
+
+for city in sheet_data:
+    if city["iataCodes"] == '':
+        city["iataCodes"] = flight_search.get_destination_code(city['city'])
+print(f"sheet_data:\n {sheet_data}")
+
+data_manager.datarow = sheet_data
+data_manager.edit_exiting_record()
+
+
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+
