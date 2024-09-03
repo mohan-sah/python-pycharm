d88eee56e0a2f3b5762c8a4ffa69e397db2376b4
mohan-sah <mohansah944@gmail.com>
2024-09-03 21:27:54 +0530

    bot basic struct done

--- web_python_small_projects/twitter_complaint_bot/main.py
+++ web_python_small_projects/twitter_complaint_bot/main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from selenium import webdriver
 load_dotenv()
 
 PROMISED_UP = 40
@@ -9,4 +10,22 @@ TWITTER_EMAIL =  os.environ["TWITTER_EMAIL"]
 TWITTER_PASSWORD =  os .environ["TWITTER_PASSWORD"]
 
 class InternetSpeedTwitterBot:
-    pass
+    def __init__(self):
+        self.up = None
+        self.down = None
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--lang=en")
+        chrome_options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+    def get_internet_speed(self):
+
+        pass
+
+    def tweet_at_provider(self):
+        pass
+
+if __name__ == '__main__':
+    bot = InternetSpeedTwitterBot()
+    bot.get_internet_speed()
+    bot.tweet_at_provider()
