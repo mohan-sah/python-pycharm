45f0f5cd9bb2b77b642d65cecce19d714f6b9f8a
mohan-sah <mohansah944@gmail.com>
2024-09-03 21:19:00 +0530

    twitter bot start

---
+++ web_python_small_projects/twitter_complaint_bot/main.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+PROMISED_UP = 40
+PROMISED_DOWN = 40
+
+TWITTER_EMAIL =  os.environ["TWITTER_EMAIL"]
+TWITTER_PASSWORD =  os .environ["TWITTER_PASSWORD"]
+
+class InternetSpeedTwitterBot:
+    pass
