1acd4b32e58ad529197d12ad7478ab41312eba36
mohan-sah <mohansah944@gmail.com>
2024-09-06 23:08:59 +0530

    added header data for soup

---
+++ web_python_small_projects/data_entry_house_listing/http_header.py
@@ -0,0 +1,24 @@
+
+#'Host':'myhttpheader.com'
+# or from https://httpbin.org/headers
+class HttpHeader:
+    def __init__(self):
+        self.header_data = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7",
+        "Cache-Control": "max-age=0",
+        "Dnt": "1",
+        "Priority": "u=0, i",
+        "Sec-Ch-Ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Google Chrome\";v=\"128\"",
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": "\"Windows\"",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"}
+
+
+
--- web_python_small_projects/data_entry_house_listing/main.py
+++ web_python_small_projects/data_entry_house_listing/main.py
@@ -1,18 +1,18 @@
-import time
-import os
+import time,os
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from http_header import HttpHeader
 
 load_dotenv()
-
+http_header = HttpHeader()
 FORM_URL = os.environ['FORM_URL']
 SITE_URL = os.environ['SITE_URL']
-response = requests.get(SITE_URL)
-content = response.text
 
+response = requests.get(SITE_URL,headers=http_header.header_data)
+content = response.text
 soup = BeautifulSoup(content,"html.parser")
 webpage_title = soup.title.text
 print(webpage_title)
@@ -20,12 +20,8 @@ print(webpage_title)
 cards = soup.find(name='div', id = 'grid-search-results')
 
 listing_links = [listing.get('href') for listing in cards.select('a[data-test="property-card-link"]')]
-
 prices = [price.text.split('+')[0].split('/')[0] for price in cards.select('span[data-test="property-card-price"]')]
 addresses = [address.text.strip().replace('|','') for address in cards.select('address[data-test="property-card-addr"]')] #
-print(f'{listing_links}')
-print(f'{prices}')
-print(f"{addresses}")
 housing_list = [[link,price,address] for link,price,address in zip(listing_links,prices,addresses)]
 print(housing_list)
 
