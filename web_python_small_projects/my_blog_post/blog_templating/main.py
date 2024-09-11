7ac4d1d3a9ca15ecfd6b82a36720e2df0b26cf47
mohan-sah <mohansah944@gmail.com>
2024-09-11 12:55:01 +0530

    blog templating , post generataed with api, successfully displayed over various urls

--- web_python_small_projects/my_blog_post/blog_templating/main.py
+++ web_python_small_projects/my_blog_post/blog_templating/main.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
-
+all_post_response = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 @app.route('/')
 def home():
-    return render_template("index.html")
+    # all_post_response = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+    return render_template("index.html", all_post =all_post_response)
+
+@app.route('/post/<int:post_id>')
+def view_post(post_id):
+    clicked_post = all_post_response[post_id-1]
+    return render_template("post.html", post =clicked_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
