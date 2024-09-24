8e22a517b117aebb6e10805718c9dd82f8d86d72
mohan-sah <mohansah944@gmail.com>
2024-09-24 22:06:51 +0530

    upgraded blog start

---
+++ web_python_small_projects/upgraded_blog/main.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+
+
+app = Flask(__name__)
+print(__name__.split)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
