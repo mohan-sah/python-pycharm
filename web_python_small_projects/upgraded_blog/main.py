ab43abc5be97dd5c15d947effd6eb135ce27510f
mohan-sah <mohansah944@gmail.com>
2024-10-01 11:05:04 +0530

    connected to data bin

--- web_python_small_projects/upgraded_blog/main.py
+++ web_python_small_projects/upgraded_blog/main.py
@@ -1,5 +1,9 @@
 from flask import Flask,render_template
+import requests
+ 
 
+# take data from here "https://api.npoint.io/7495458f5307404ee89f"
+response = requests.get("https://api.npoint.io/7495458f5307404ee89f").json()
 
 app = Flask(__name__)
 print(__name__.split)
@@ -7,7 +11,21 @@ print(__name__.split)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    
+    return render_template('index.html', all_blog_post = response)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/post')
+def post():
+    return render_template('post.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
