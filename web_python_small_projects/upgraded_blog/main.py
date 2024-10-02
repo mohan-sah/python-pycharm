f88490954fd0d842057b512ac0d77aa78841dea8
mohan-sah <mohansah944@gmail.com>
2024-10-02 22:30:19 +0530

    contact form redirect corrected

--- web_python_small_projects/upgraded_blog/main.py
+++ web_python_small_projects/upgraded_blog/main.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import requests
+from notification_manager import NotificationManager
  
 
 # take data from here "https://api.npoint.io/7495458f5307404ee89f"
@@ -7,7 +8,7 @@ response = requests.get("https://api.npoint.io/7495458f5307404ee89f").json()
 
 app = Flask(__name__)
 print(__name__.split)
-
+notify = NotificationManager()
 
 @app.route('/')
 def home():
@@ -18,13 +19,29 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+
 
 @app.route('/post/<int:id>')
 def post(id):
     return render_template('post.html',post_id=id, all_blog_post = response)
+
+# @app.route('/contact')
+# def contact():
+#     return render_template('contact.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        print(data)
+        name = data['name']
+        email =data['email']
+        phoneno =data['phoneno']
+        message =data['message']
+        print(name, email, phoneno, message)
+        notify.send_emails(receive_mail="mohansah.100daysofcode@gmail.com", message_body=f"Subject: MY WEBSITE \n\nname = {data['name']}\nemail ={data['email']}\nphoneno ={data['phoneno']}message ={data['message']}")
+        return render_template('contact.html',msg_sent = True)
+    else: return render_template('contact.html',msg_sent = False)
 
 
 if __name__ == '__main__':
